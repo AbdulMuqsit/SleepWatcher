8236d8c8b7f57190498f22f5082c6103725f2026
AbdulMuqsit <abdemuqsit@hotmail.com>
2014-12-12 14:01:45 +0500

    Made c# 5 compatible

--- SleepWatcher/SleepWatcher.Entites/Step.cs
+++ SleepWatcher/SleepWatcher.Entites/Step.cs
@@ -57,8 +57,8 @@ namespace SleepWatcher.Entites
             }
         }
         [NotMapped]
-        public string DueDate => AlarmTime.ToShortDateString();
-        public string DateStarted => DateAdded.ToShortDateString();
+        public string DueDate {get{return AlarmTime.ToShortDateString();}} 
+        public string DateStarted{get{return DateAdded.ToShortDateString();}} 
         public virtual ICollection<Note> Notes { get; set; }
 
     }
