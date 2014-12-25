8501405e980744a58bb2323b6c4e478218f7ec67
AbdulMuqsit <abdulmuqsitbutt@live.com>
2014-12-25 20:12:16 +0500

    Implemented onpropertychanged on new view models

--- SleepWatcher/SleepWatcher/ViewModel/PatientViewModel/NotesViewModel.cs
+++ SleepWatcher/SleepWatcher/ViewModel/PatientViewModel/NotesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,43 @@ using SleepWatcher.Model;
 
 namespace SleepWatcher.ViewModel.PatientViewModel
 {
-    class NotesViewModel
+    class NotesViewModel : ViewModelBase
     {
-        public RangeObservableCollection<NoteModel> Notes { get; set; }
-        public PatientModel Patient { get; set; }
-        public StepModel Step { get; set; }
+        private RangeObservableCollection<NoteModel> _notes;
+        private PatientModel _patient;
+        private StepModel _step;
+
+        public RangeObservableCollection<NoteModel> Notes
+        {
+            get { return _notes; }
+            set
+            {
+                if (Equals(value, _notes)) return;
+                _notes = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PatientModel Patient
+        {
+            get { return _patient; }
+            set
+            {
+                if (Equals(value, _patient)) return;
+                _patient = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StepModel Step
+        {
+            get { return _step; }
+            set
+            {
+                if (Equals(value, _step)) return;
+                _step = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
--- SleepWatcher/SleepWatcher/ViewModel/PatientViewModel/SingleNoteViewModel.cs
+++ SleepWatcher/SleepWatcher/ViewModel/PatientViewModel/SingleNoteViewModel.cs
@@ -7,9 +7,31 @@ using SleepWatcher.Model;
 
 namespace SleepWatcher.ViewModel.PatientViewModel
 {
-    class SingleNoteViewModel
+    class SingleNoteViewModel:ViewModelBase
     {
-        public NoteModel Note { get; set; }
-        public int StepId { get; set; }
+        private NoteModel _note;
+        private int _stepId;
+
+        public NoteModel Note
+        {
+            get { return _note; }
+            set
+            {
+                if (Equals(value, _note)) return;
+                _note = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int StepId
+        {
+            get { return _stepId; }
+            set
+            {
+                if (Equals(value, _stepId)) return;
+                _stepId = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
